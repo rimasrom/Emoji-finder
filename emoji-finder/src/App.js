a067070888e5b9ea3aca880dc44d81d5ec2d13bc
rimasrom <hembromsamir715@gmail.com>
2023-03-17 19:49:47 +0530

    refactor

--- emoji-finder/src/App.js
+++ emoji-finder/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import './style.css'
 
 const App = () => {
   return (
-    <>
+    <div className="container-main">
       
-    </>
+        <div className="header">
+        <h1>What emoji it is</h1>
+        </div>
+
+        <div className="input">
+          <input type="text" />
+        </div>
+
+        <div className="output">
+        <h3>The result will be displayed here</h3>
+        </div>
+
+      
+    </div>
   )
 }
 
